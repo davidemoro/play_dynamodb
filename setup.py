5f884646d8f8811533e37943c613a242f13f1b41
Davide Moro <dmoro@tierratelematics.com>
2018-01-31 11:14:06 +0100

    Back to development: 0.0.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='play_dynamodb',
-    version='0.0.1',
+    version='0.0.2.dev0',
     description="pytest-play support for AWS DynamoDB queries and assertions",
     long_description=readme + '\n\n' + history,
     author="Davide Moro",
